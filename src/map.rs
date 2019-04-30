229f2602e1a5067c8a92c6381cdfaa01b7ca5ab2
MicroChasm <joel.gm.ryan@gmail.com>
2019-04-29 22:23:34 -0700

    changed orc and troll colors.  Also changed the colors in the map file to yellow because I don't understand how to actually use the new colors

--- src/map.rs
+++ src/map.rs
@@ -143,14 +143,14 @@ pub fn make_island(map: &mut Map, objects: &mut Vec<Object>, config: &Config) ->
 
         if !map.is_blocked(x, y, objects) {
             let mut monster = if rand::random::<f32>() < 1.0 {
-                let mut orc = Object::new(x, y, 'o', "orc", DESATURATED_GREEN, true);
+                let mut orc = Object::new(x, y, 'o', "orc", YELLOW, true);
                 orc.fighter = Some(Fighter{max_hp: 10, hp: 10, defense: 0, power: 3, on_death: DeathCallback::Monster });
                 orc.ai = Some(Ai::Basic);
                 orc.behavior = Some(Behavior::Idle);
                 orc.color = config.color_orc.color();
                 orc
             } else {
-                let mut troll = Object::new(x, y, 'T', "troll", DARKER_GREEN, true);
+                let mut troll = Object::new(x, y, 'T', "troll", YELLOW, true);
                 troll.fighter = Some(Fighter{max_hp: 16, hp: 16, defense: 1, power: 4, on_death: DeathCallback::Monster });
                 troll.ai = Some(Ai::Basic);
                 troll.behavior = Some(Behavior::Idle);
