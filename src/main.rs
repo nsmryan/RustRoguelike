15b2912730cdd949686bff2976145416577341c1
nsmryan <nsmryan@gmail.com>
2019-05-01 22:23:34 -0400

    unwrap -> expect for config files'

--- src/main.rs
+++ src/main.rs
@@ -59,8 +59,7 @@ fn handle_keys(game: &mut Game,
             player_move_or_attack(0, 1, map, objects, messages);
             TookTurn
         }
-
-        (Key { code: Left,    .. }, true) |
+(Key { code: Left,    .. }, true) |
         (Key { code: Number4, .. }, true) |
         (Key { code: NumPad4, .. }, true) => {
             player_move_or_attack(-1, 0, map, objects, messages);
@@ -289,7 +288,7 @@ fn main() {
     {
         let mut file = File::open("config.json").expect("Could not open/parse config file config.json");
         let mut config_string = String::new();
-        file.read_to_string(&mut config_string).unwrap();
+        file.read_to_string(&mut config_string).expect("Could not parse config.json file!");
         config = serde_json::from_str(&config_string).unwrap();
     }
 
