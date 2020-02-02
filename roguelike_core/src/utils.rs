f8d15b6dc5e35003d5bab7526e894ee8c2559974
nsmryan <nsmryan@gmail.com>
2020-02-01 23:18:54 -0500

    Attack msg occurs even if target was killed in the same attack

--- roguelike_core/src/utils.rs
+++ roguelike_core/src/utils.rs
@@ -15,7 +15,6 @@ pub fn attack(handle: ObjectId, other_handle: ObjectId, objects: &mut ObjMap, ms
         objects[other_handle].take_damage(damage);
 
         msg_log.log(Msg::Attack(handle, other_handle, damage));
-
         if objects[other_handle].fighter.unwrap().hp <= 0 {
             objects[other_handle].alive = false;
 
