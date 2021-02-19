52931b2008941a0499a87a03e875f8f354e48fde
Noah Ryan <nsmryan@gmail.com>
2021-02-18 21:01:41 -0500

    Initial commands in a simple interpreter

--- roguelike_lib/src/commands.rs
+++ roguelike_lib/src/commands.rs
@@ -1,6 +1,65 @@
+use std::str::FromStr;
+
+use roguelike_core::types::*;
+use roguelike_engine::game::*;
 
 
-pub fn commands() {
-    println!("Commands");
+pub enum Command {
+    PlayerId,
+    Pos(u64),
+    SetPos(u64, i32, i32),
+    Exit,
+}
+
+impl FromStr for Command {
+    type Err = String;
+
+    fn from_str(string: &str) -> Result<Self, Self::Err> {
+        let s: &mut str = &mut string.to_string();
+        s.make_ascii_lowercase();
+
+        let args = s.split(" ").collect::<Vec<&str>>();
+        let cmd = args[0];
+
+        if cmd == "player_id" {
+            return Ok(Command::PlayerId);
+        } else if cmd == "pos" {
+            let id = args[1].parse::<u64>().unwrap();
+            if args.len() == 2 {
+                return Ok(Command::Pos(id));
+            } else {
+                let x  = args[2].parse::<i32>().unwrap();
+                let y  = args[3].parse::<i32>().unwrap();
+                return Ok(Command::SetPos(id, x, y));
+            }
+        } else if cmd == "exit" {
+            return Ok(Command::Exit);
+        }
+        panic!(format!("Command {} not expected!", cmd));
+    }
+}
+
+pub fn execute(command: &Command, game: &mut Game) -> String {
+    match command {
+        Command::PlayerId => {
+            let player_id = game.data.find_by_name(EntityName::Player).unwrap();
+            return player_id.to_string();
+        }
+
+        Command::Pos(id) => {
+            let pos = game.data.entities.pos[id];
+            return format!("{} {}", pos.x, pos.y);
+        }
+
+        Command::SetPos(id, x, y) => {
+            game.data.entities.pos[id] = Pos::new(*x, *y);
+            return "".to_string();
+        }
+
+        Command::Exit => {
+            game.settings.running = false;
+            return "".to_string();
+        }
+    }
 }
 
--- roguelike_lib/src/main.rs
+++ roguelike_lib/src/main.rs
@@ -1,9 +1,31 @@
 mod commands;
 
+use std::io;
+use std::io::{BufRead};
+
+use roguelike_core::config::*;
+use roguelike_engine::game::*;
+
 use crate::commands::*;
 
 
+const CONFIG_NAME: &str = "config.yaml";
+
 pub fn main() {
-    println!("Main");
+    let stdin = io::stdin();
+    let mut iter = stdin.lock().lines();
+
+    let config = Config::from_file(CONFIG_NAME);
+    let mut game = Game::new(0, config).unwrap();
+
+    while game.settings.running {
+        let msg = iter.next().unwrap().unwrap();
+
+        let cmd = msg.parse::<Command>()
+                     .expect(&format!("Unexpected command {}", msg));
+
+        let result = execute(&cmd, &mut game);
+        println!("{}", result);
+    }
 }
 
